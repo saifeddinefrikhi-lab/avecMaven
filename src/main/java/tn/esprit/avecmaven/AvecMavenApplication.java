a773c04f21895428cb0b9737e3ab5b34ffb201be
saifeddinefrikhi-lab <saifeddine.frikhi@esprit.tn>
2025-11-18 23:05:26 +0100

    Initial commit - Projet Maven avec structure de base

---
+++ src/main/java/tn/esprit/avecmaven/AvecMavenApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.avecmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvecMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvecMavenApplication.class, args);
+    }
+
+}
